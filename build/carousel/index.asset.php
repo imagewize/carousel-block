13e2fd5a9aacb0cfaf6accfbfd07f7d470620995
Jasper Frumau <jasper@imagewize.com>
2025-01-30 18:31:42 +0700

    style.scss loading update

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4aef6145a8f504fe6cdf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd313a4a39ac9549fe9b5');
--- src/carousel/index.js
+++ src/carousel/index.js
@@ -1,8 +1,20 @@
 import { registerBlockType } from '@wordpress/blocks';
-import metadata from './block.json';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * All files containing `style` keyword are bundled together. The code used
+ * gets applied both to the front of your site and to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
 import './style.scss';
+
+/**
+ * Internal dependencies
+ */
 import edit from './edit';
 import save from './save';
+import metadata from './block.json';
 
 const CarouselIcon = (
     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false">
@@ -11,6 +23,11 @@ const CarouselIcon = (
     </svg>
 );
 
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
 registerBlockType('cb/carousel', {
     ...metadata,
     icon: CarouselIcon,
