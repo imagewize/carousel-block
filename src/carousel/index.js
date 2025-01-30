f74662fd44a148fb0fae0c6fc50f7bcf190861e1
Jasper Frumau <jasper@imagewize.com>
2025-01-30 09:08:12 +0700

    first commit

---
+++ src/carousel/index.js
@@ -0,0 +1,91 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, InnerBlocks, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, RangeControl, ToggleControl, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import classnames from 'classnames';
+
+import './editor.scss';
+
+const ALLOWED_BLOCKS = ['cb/slide'];
+
+const CarouselIcon = (
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false">
+        <path d="M7 19h10V4H7v15zm-5-2h4V6H2v11zM18 6v11h4V6h-4z" />
+        <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+);
+
+registerBlockType('cb/carousel', {
+    icon: CarouselIcon,
+    edit: function Edit({ attributes, setAttributes, clientId }) {
+        const {
+            slidesToShow,
+            slidesToScroll,
+            speed,
+            arrows,
+            dots,
+            infinite,
+            autoplay,
+            autoplaySpeed,
+            rtl,
+            responsiveWidth,
+            responsiveSlides,
+            responsiveSlidesToScroll,
+        } = attributes;
+
+        const slideCount = useSelect(
+            (select) => select('core/block-editor').getBlock(clientId).innerBlocks.length
+        );
+
+        const blockProps = useBlockProps({
+            className: classnames(
+                `cb-shows-${slidesToShow}-slides`,
+                slideCount + 1 > slidesToShow ? 'cb-show-scrollbar' : 'cb-hide-scrollbar'
+            ),
+        });
+
+        const placeholder = (
+            <div className="cb-carousel-placeholder">
+                {__('Click plus (+) to add slides', 'cb')}
+            </div>
+        );
+
+        return (
+            <Fragment>
+                <InspectorControls>
+                    <PanelBody title={__('Carousel Settings', 'cb')} initialOpen={true}>
+                        {/* Inspector controls implementation */}
+                    </PanelBody>
+                    <PanelBody title={__('Responsive Settings', 'cb')} initialOpen={false}>
+                        {/* Responsive settings implementation */}
+                    </PanelBody>
+                </InspectorControls>
+                <div {...blockProps}>
+                    <InnerBlocks
+                        orientation="horizontal"
+                        allowedBlocks={ALLOWED_BLOCKS}
+                        templateLock={false}
+                        renderAppender={InnerBlocks.ButtonBlockAppender}
+                        placeholder={placeholder}
+                    />
+                </div>
+            </Fragment>
+        );
+    },
+    save: function Save({ attributes }) {
+        // Save implementation
+        const blockProps = useBlockProps.save({
+            className: classnames({ 'cb-single-slide': slidesToShow == 1 }),
+            'data-slick': JSON.stringify(slickSettings),
+            dir: rtl ? 'rtl' : undefined,
+        });
+
+        return (
+            <div {...blockProps}>
+                <InnerBlocks.Content />
+            </div>
+        );
+    },
+});
---
+++ src/slide/index.js
@@ -0,0 +1,53 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+
+import './editor.scss';
+
+const SlideIcon = (
+    <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-hidden="true"
+        focusable="false"
+    >
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="m18 6v11h4v-11h-4z" />
+        <path d="m2 17h4v-11h-4v11z" />
+        <path d="m7.0059 4v15.004h9.9902v-15.004h-9.9902zm1.3574 1.3555h7.2773v12.291h-7.2773v-12.291z" />
+    </svg>
+);
+
+registerBlockType('cb/slide', {
+    icon: SlideIcon,
+    edit: function Edit({ clientId }) {
+        const blockProps = useBlockProps();
+        const { hasChildBlocks } = useSelect((select) => {
+            const { getBlockOrder } = select('core/block-editor');
+            return {
+                hasChildBlocks: getBlockOrder(clientId).length > 0,
+            };
+        }, [clientId]);
+
+        return (
+            <div {...blockProps}>
+                <InnerBlocks
+                    templateLock={false}
+                    renderAppender={
+                        !hasChildBlocks ? InnerBlocks.ButtonBlockAppender : undefined
+                    }
+                />
+            </div>
+        );
+    },
+    save: function Save() {
+        const blockProps = useBlockProps.save();
+        return (
+            <div {...blockProps}>
+                <InnerBlocks.Content />
+            </div>
+        );
+    },
+});
