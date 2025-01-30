ecc50404f2eb5c3219f4bdbf328aed65a25b544b
Jasper Frumau <jasper@imagewize.com>
2025-01-30 16:05:26 +0700

    editor inpector controls

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1fa34ab2a051c636f313');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7557c6ebf3a19474887d');
--- src/carousel/edit.js
+++ src/carousel/edit.js
@@ -25,7 +25,6 @@ export default function Edit({ attributes, setAttributes, clientId }) {
         responsiveSlidesToScroll,
     } = attributes;
 
-    // ...existing code from original edit function...
     const slideCount = useSelect(
         (select) => select('core/block-editor').getBlock(clientId).innerBlocks.length
     );
@@ -47,10 +46,87 @@ export default function Edit({ attributes, setAttributes, clientId }) {
         <Fragment>
             <InspectorControls>
                 <PanelBody title={__('Carousel Settings', 'cb')} initialOpen={true}>
-                    // ...existing controls code...
+                    <RangeControl
+                        label={__('Slides to Show', 'cb')}
+                        value={slidesToShow}
+                        onChange={(value) => setAttributes({ slidesToShow: value })}
+                        min={1}
+                        max={10}
+                    />
+                    <RangeControl
+                        label={__('Slides to Scroll', 'cb')}
+                        value={slidesToScroll}
+                        onChange={(value) => setAttributes({ slidesToScroll: value })}
+                        min={1}
+                        max={10}
+                    />
+                    <RangeControl
+                        label={__('Animation Speed (ms)', 'cb')}
+                        value={speed}
+                        onChange={(value) => setAttributes({ speed: value })}
+                        min={100}
+                        max={3000}
+                        step={100}
+                    />
+                    <ToggleControl
+                        label={__('Show Arrows', 'cb')}
+                        checked={arrows}
+                        onChange={(value) => setAttributes({ arrows: value })}
+                    />
+                    <ToggleControl
+                        label={__('Show Dots', 'cb')}
+                        checked={dots}
+                        onChange={(value) => setAttributes({ dots: value })}
+                    />
+                    <ToggleControl
+                        label={__('Infinite Loop', 'cb')}
+                        checked={infinite}
+                        onChange={(value) => setAttributes({ infinite: value })}
+                    />
+                    <ToggleControl
+                        label={__('Autoplay', 'cb')}
+                        checked={autoplay}
+                        onChange={(value) => setAttributes({ autoplay: value })}
+                    />
+                    {autoplay && (
+                        <RangeControl
+                            label={__('Autoplay Speed (ms)', 'cb')}
+                            value={autoplaySpeed}
+                            onChange={(value) => setAttributes({ autoplaySpeed: value })}
+                            min={1000}
+                            max={10000}
+                            step={500}
+                        />
+                    )}
+                    <ToggleControl
+                        label={__('RTL Mode', 'cb')}
+                        checked={rtl}
+                        onChange={(value) => setAttributes({ rtl: value })}
+                    />
                 </PanelBody>
                 <PanelBody title={__('Responsive Settings', 'cb')} initialOpen={false}>
-                    // ...existing responsive controls code...
+                    <RangeControl
+                        label={__('Breakpoint Width (px)', 'cb')}
+                        value={responsiveWidth}
+                        onChange={(value) => setAttributes({ responsiveWidth: value })}
+                        min={320}
+                        max={1200}
+                        step={1}
+                    />
+                    <RangeControl
+                        label={__('Slides to Show (Mobile)', 'cb')}
+                        value={responsiveSlides}
+                        onChange={(value) => setAttributes({ responsiveSlides: value })}
+                        min={1}
+                        max={5}
+                    />
+                    <RangeControl
+                        label={__('Slides to Scroll (Mobile)', 'cb')}
+                        value={responsiveSlidesToScroll}
+                        onChange={(value) => setAttributes({ responsiveSlidesToScroll: value })}
+                        min={1}
+                        max={5}
+                    />
                 </PanelBody>
             </InspectorControls>
             <div {...blockProps}>
