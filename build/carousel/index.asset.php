76be8985f2af7044ee6e8ea6e3cb6ad83f81ed61
Jasper Frumau <jasper@imagewize.com>
2025-01-30 16:01:52 +0700

    meta data loading change

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e878b61fa881b1330fa6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1fa34ab2a051c636f313');
--- src/carousel/index.js
+++ src/carousel/index.js
@@ -1,7 +1,6 @@
 import { registerBlockType } from '@wordpress/blocks';
-
+import metadata from './block.json';
 import './style.scss';
-
 import edit from './edit';
 import save from './save';
 
@@ -13,57 +12,8 @@ const CarouselIcon = (
 );
 
 registerBlockType('cb/carousel', {
+    ...metadata,
     icon: CarouselIcon,
-    attributes: {
-        slidesToShow: {
-            type: 'number',
-            default: 3
-        },
-        slidesToScroll: {
-            type: 'number',
-            default: 1
-        },
-        arrows: {
-            type: 'boolean',
-            default: true
-        },
-        dots: {
-            type: 'boolean',
-            default: true
-        },
-        infinite: {
-            type: 'boolean',
-            default: true
-        },
-        autoplay: {
-            type: 'boolean',
-            default: false
-        },
-        autoplaySpeed: {
-            type: 'number',
-            default: 3000
-        },
-        speed: {
-            type: 'number',
-            default: 500
-        },
-        rtl: {
-            type: 'boolean',
-            default: false
-        },
-        responsiveWidth: {
-            type: 'number',
-            default: 768
-        },
-        responsiveSlides: {
-            type: 'number',
-            default: 1
-        },
-        responsiveSlidesToScroll: {
-            type: 'number',
-            default: 1
-        }
-    },
     edit,
     save
 });
