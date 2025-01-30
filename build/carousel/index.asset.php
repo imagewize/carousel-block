8bcdb3e4e0499aeb0121a08785e1dcdf807480ab
Jasper Frumau <jasper@imagewize.com>
2025-01-30 16:22:17 +0700

    index.css

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7557c6ebf3a19474887d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '932cd9af93596cb8cc44');
